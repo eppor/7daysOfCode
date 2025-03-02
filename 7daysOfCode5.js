654207885405219d2207e573357e37277f5880bd
eppor <emiliaprzybyszewska@gmail.com>
2025-03-02 13:20:36 -0300

    Adicionada remocao dos itens para desafio do dia 5

--- 7daysOfCode5.js
+++ 7daysOfCode5.js
@@ -6,8 +6,13 @@ let congelados = [];
 let doces = [];
 let bebidas = [];
 
+
 let continuar = prompt("Você deseja adicionar uma comida na sua lista de compras?\nsim\nnao").toLowerCase();
-while(continuar === "sim") {
+if (continuar === "sim") {
+    adicionarItem();
+}
+function adicionarItem() {
+    while(continuar === "sim") {
     let comida = prompt("Qual comida você deseja inserir?");
     let categoriaEscolhida = prompt(`Em qual categoria essa comida se encaixa?:\nVerduras\nLegumes\nFrutas\nLaticinios\nCongelados\nDoces\nBebidas`).toLowerCase();
     if(categoriaEscolhida === "verduras") {
@@ -30,14 +35,47 @@ while(continuar === "sim") {
     }
 
     continuar = prompt("Voce deseja adicionar mais alguma coisa?");
+    }
 }
 
-let exibirListaDeCompras = alert(`Lista de compras pra hoje:\n
-                                Verduras: ${verduras}\n
-                                Legumes: ${legumes}\n
-                                Frutas: ${frutas}\n
-                                Laticinios: ${laticinios}\n
-                                Congelados: ${congelados}\n
-                                Doces: ${doces}\n
-                                Bebidas: ${bebidas}.`);
 
+let remover = prompt("Voce deseja remover algum item?");
+while(remover === "sim") {
+    let oQueRemover = prompt(`Qual item voce deseja remover?\n
+                        Verduras: ${verduras}\n
+                        Legumes: ${legumes}\n
+                        Frutas: ${frutas}\n
+                        Laticinios: ${laticinios}\n
+                        Congelados: ${congelados}\n
+                        Doces: ${doces}\n
+                        Bebidas: ${bebidas}?`);
+    removerItem(oQueRemover);
+    remover = prompt("Você deseja remover algum outro item?");
+    }
+
+function removerItem(item) {
+    let listaDeCategorias = { verduras, legumes, frutas, laticinios, congelados, doces, bebidas };
+
+    for (let categoria in listaDeCategorias) {
+        let existe = listaDeCategorias[categoria].indexOf(item);
+        if (existe !== -1) {
+            listaDeCategorias[categoria].splice(existe, 1);
+            alert(`${item} foi removido da lista.`);
+            return;
+        }
+    }
+    alert("Não foi possível encontrar o item dentro da lista!");
+}
+
+exibirListaDeCompras();
+
+function exibirListaDeCompras () {
+   let exibir = alert(`Lista de compras pra hoje:\n
+    Verduras: ${verduras}\n
+    Legumes: ${legumes}\n
+    Frutas: ${frutas}\n
+    Laticinios: ${laticinios}\n
+    Congelados: ${congelados}\n
+    Doces: ${doces}\n
+    Bebidas: ${bebidas}.`);
+}
